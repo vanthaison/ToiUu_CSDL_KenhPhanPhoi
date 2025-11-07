a81af48d18a208556ce61ce443d98753cc4b6e04
vanthaison <vanthaison2008.tp@gmail.com>
2025-11-07 10:45:58 +0700

    Delete KHDT.sql

--- KHDT.sql
+++
@@ -1,6 +0,0 @@
-﻿USE PCGaming; -- hoặc tên database của bạn, ví dụ PCGaming
-GO
-delete from KhachHang_DacThu
-INSERT INTO KhachHang_DacThu (MaDoiTuong, TenKH, LoaiUuDai, GhiChu)
-VALUES
-('DT03', N'Trung tâm Thể thao Quốc Gia', N'Hợp đồng dài hạn', N'Khách hàng ưu tiên thanh toán chậm 30 ngày.');
